4cb154151c8bf0ce26d548a5eecfb3a212fec9b9
ibrerdogan <94644110+ibrerdogan@users.noreply.github.com>
2023-07-30 13:43:29 +0300

    added userdeailview

---
+++ FacebookFriends/View/UserDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UserDetailViewController.swift
+//  FacebookFriends
+//
+//  Created by İbrahim Erdogan on 30.07.2023.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+    
+}
